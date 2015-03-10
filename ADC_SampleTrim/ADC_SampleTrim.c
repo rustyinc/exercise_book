8e805b015b3edc89df9e07eee9289496786acc6b
Ruben Sternke <ruben@sternke.dk>
2015-03-10 20:32:12 +0100

    Implemented program, that reads in the adc, divide the result by four and outputs the result on the 8 LEDs

--- ADC_SampleTrim/ADC_SampleTrim.c
+++ ADC_SampleTrim/ADC_SampleTrim.c
@@ -1,10 +1,71 @@
+/****************************************************************************
+ * Project Name:    ADC sample Trimresistor
+ * Author:          RUST
+ * Filename:        ADC_SampleTrim.c
+ * Purpose:         Class exercise
+ * ------------------------------------------------------------------------
+ * Version history:
+ * 10-03-15: Initial version
+ *
+ * =========================================================================
+ * Description:
+ * This program samples the input from the trimresistor on the LEDpad, and
+ * outputs a number between 0 and 255 according to the position of the
+ * trimresistor
+ *
+ * Required:
+ * -> LaunchPad
+ * -> LED Pad
+ *
+ * Port 2.x Output
+ * Port 1.1 Input (trim resisor)
+ ****************************************************************************/
+
 #include <msp430.h> 
 
-/*
- * main.c
- */
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+int main(void)
+{
+    /* Stop the watchdog timer */
+    WDTCTL = WDTPW | WDTHOLD;
+
+    /**
+     * Setup entire port 2 as output
+     * BIT6 and BIT7 cleared in select for enabling GPIO
+     */
+    P2DIR  |= (BIT7 | BIT6 | BIT5 | BIT4 | BIT3 | BIT2 | BIT1 | BIT0);
+    P2SEL  &= ~(BIT7 | BIT7);
+    P2SEL2 &= ~(BIT7 | BIT7);
+
+    /**
+     * Setup the ADC10CTL0
+     * ADC10SHT_2: 16xsample and hold
+     * ADC10ON   : Turn on ADC
+     * ADC10IE   : Enable ADC interrupts
+     */
+    ADC10CTL0 = ADC10SHT_2 + ADC10ON + ADC10IE;
+    /* Set ADC input to P1.x */
+    ADC10CTL1 = INCH_5;
+    /* Enable analog input on P1.x */
+    ADC10AE0 |= BIT4;
+
+    /* Enable and start conversion */
+    ADC10CTL0 |= ENC + ADC10SC;
+
+    /* Enable interrupts globally - and go to sleep, zzzzzzz */
+    __bis_SR_register(GIE + LPM0_bits);
 	
 	return 0;
+}
+
+/**
+ *  ADC10 interrupt service routine
+ */
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR(void)
+{
+    /* Divide result by four and output on LEDs */
+    P2OUT = ADC10MEM << 2;
+
+    /* Restart conversion */
+    ADC10CTL0 |= ENC + ADC10SC;
 }
